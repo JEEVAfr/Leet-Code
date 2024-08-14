9cd82e95d848f9170a21328c328b693757e782f2
JEEVAfr <146312723+JEEVAfr@users.noreply.github.com>
2024-08-14 12:22:05 +0530

    Time: 98 ms (80.04%), Space: 16.7 MB (17.87%) - LeetHub

---
+++ 2816-lexicographically-smallest-palindrome/2816-lexicographically-smallest-palindrome.py
@@ -0,0 +1,26 @@
+class Solution:
+    def makeSmallestPalindrome(self, s: str) -> str:
+        s = list(s)
+        left = 0
+        right = len(s) - 1
+
+        while left <= right:
+            
+            if s[left] != s[right]:
+                # Make both characters equal to the smaller one
+                if s[left] < s[right]:
+                    s[right] = s[left]
+                else:
+                    s[left] = s[right]
+            left += 1
+            right -= 1
+
+        return ''.join(s)
+
+            
+
+
+                
+        
+
+        
