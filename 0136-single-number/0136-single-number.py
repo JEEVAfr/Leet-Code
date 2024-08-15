9ddf75d0cb2ecc670a9814bc7c572f6582c3dfad
JEEVA <146312723+JEEVAfr@users.noreply.github.com>
2024-08-15 18:02:21 +0530

    Time: 111 ms (53.37%), Space: 19 MB (89.4%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,13 +1,22 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        counter = {}
+        #counter = {}
 
-        for i in nums:
-            counter[i] = counter.get(i, 0) + 1
+        #for i in nums:
+        #   counter[i] = counter.get(i, 0) + 1
         
 
-        for key, value in counter.items():
-            if value == 1:
-                return key
+        #for key, value in counter.items():
+        #    if value == 1: # 0(n) time
+        #        return key # 0(n) space
+
+
+        unique = 0
+
+        for i in nums:
+            unique = unique ^ i
+        return unique
+
+        
         
 
