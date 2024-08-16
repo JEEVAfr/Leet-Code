0bd83e31a410930387074692fe998d4bde81bfae
JEEVA <146312723+JEEVAfr@users.noreply.github.com>
2024-08-16 13:51:42 +0530

    Time: 271 ms (56.54%), Space: 16.4 MB (95.76%) - LeetHub

--- 2566-number-of-unequal-triplets-in-array/2566-number-of-unequal-triplets-in-array.py
+++ 2566-number-of-unequal-triplets-in-array/2566-number-of-unequal-triplets-in-array.py
@@ -4,7 +4,7 @@ class Solution:
         for i in range(len(nums)):
             for j in range(i +1, len(nums)):
                 for k in range(j + 1, len(nums)):
-                    if nums[i] != nums[j] and nums[i] != nums[k] and nums[j] != nums[k]:
+                    if nums[i] != nums[j] and nums[j] != nums[k] and nums[i] != nums[k]:
                         count = count + 1
             
         return count
