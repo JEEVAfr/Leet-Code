4d096e185439596fa0f5bcc7d15f6e1a62abce8c
JEEVA <146312723+JEEVAfr@users.noreply.github.com>
2024-08-17 20:32:41 +0530

    Time: 288 ms (66.94%), Space: 32 MB (86.11%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -0,0 +1,36 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        fast = head
+        slow = head
+
+        # find middle of the slow
+        while fast and fast.next is not None:
+            fast = fast.next.next
+            slow = slow.next
+
+        #reverse second half
+        previous = None
+        while slow is not None:
+        
+            temp = slow.next
+            slow.next = previous
+            previous = slow
+            slow = temp
+        
+        # check 
+        left, right = head, previous # slow is going to be None and previous is behind of slow.
+
+        while right is not None:
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+        
+        return True
+
+        
