d70c5d560a1b02508297d3f6946b0ff3dbe78e80
JEEVA <146312723+JEEVAfr@users.noreply.github.com>
2024-08-21 19:52:09 +0530

    Time: 45 ms (13.77%), Space: 16.6 MB (64.46%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.py
+++ 0061-rotate-list/0061-rotate-list.py
@@ -8,33 +8,24 @@ class Solution:
         if head is None: #check the head 
             return head
 
-        length = 1 # calculate the length of the list
-        tail = head
+        length = 1
+        current = head
 
-        while tail.next is not None:
-            tail = tail.next
+        while current.next is not None:
+            current = current.next
             length += 1
-        
-        
-        k = k % length # bcoz if k is greater 
 
-        if k == 0:   # k is 0 there is not rotation
+        k = k % length
+           
+        if k == 0:
             return head
         
-        current = head # traverse 
-        for i in range(length - k - 1): # 5 - 2 = 3 first node start with 1
-            current = current.next     # 3 - 1 = 2
-        newHead = current.next
-        current.next = None
-        tail.next = head
+    
+        cur = head
+        for i in range(length - k - 1):
+            cur = cur.next
+        newHead = cur.next
+        cur.next = None
+        current.next = head
 
         return newHead
-
-
-        
-
-        
-
-
-            
-        
