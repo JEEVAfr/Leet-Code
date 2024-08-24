d4f0731747f542f7c17198792b3158ae551f2d12
JEEVA <146312723+JEEVAfr@users.noreply.github.com>
2024-08-24 09:58:01 +0530

    Time: 41 ms (12.3%), Space: 16.5 MB (30%) - LeetHub

---
+++ 2491-smallest-even-multiple/2491-smallest-even-multiple.py
@@ -0,0 +1,10 @@
+class Solution:
+    def smallestEvenMultiple(self, n: int) -> int:
+        if n % 2 == 0 and n % n == 0:
+            return n
+        
+        else:
+            a = n / n 
+            b = (a * n) * 2
+            return int(b)
+        
