9f4c74c1ae98f1adbb1d5b39e4d7c62911f28df4
JEEVA <146312723+JEEVAfr@users.noreply.github.com>
2024-08-26 22:03:11 +0530

    Time: 35 ms (69.88%), Space: 16.5 MB (20.62%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.py
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.py
@@ -1,11 +1,13 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
+
         result = []
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
                 if nums[i] == nums[j]:
                     result.append((i, j))
         
-        return len(result)
+        return len(result) #time - O(n*n)
+
 
         
