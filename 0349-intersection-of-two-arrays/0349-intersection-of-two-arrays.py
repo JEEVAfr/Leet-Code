9fa20b8cf92ec4d6f2fe6ac62f2d9178d552838f
JEEVA <146312723+JEEVAfr@users.noreply.github.com>
2024-08-29 14:30:44 +0530

    Time: 49 ms (51.82%), Space: 16.7 MB (25.19%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -8,4 +8,25 @@ class Solution:
             if i in num_2:
                 result.add(i)
         
-        return result
+        return result # unsorted array is efficient.
+
+        """
+        nums1.sort()
+        nums2.sort()
+        result = []
+        left = 0
+        right = 0
+
+        while left < len(nums1) and right < len(nums2):
+            if nums1[left] == nums2[right]:
+                result.append(nums1[left])
+                left = left + 1
+                right = right + 1
+            
+            elif nums1[left] > nums2[right]:
+                right = right + 1
+            else:
+                left = left + 1
+            
+        return set(result)
+        """ # sorted array is efficient.
