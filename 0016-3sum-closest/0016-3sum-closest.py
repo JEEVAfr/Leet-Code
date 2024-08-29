2431c39f2c26ec39bf679518f37b3491fb608c23
JEEVA <146312723+JEEVAfr@users.noreply.github.com>
2024-08-29 19:20:55 +0530

    Time: 405 ms (53.24%), Space: 16.7 MB (42.96%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -4,16 +4,15 @@ class Solution:
         result = float('inf') # this is larger value
 
         for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
             a = nums[i]
             j = i + 1
             k = len(nums) - 1
 
             while j < k:
 
-                b = nums[j]
-                c = nums[k]
-
-                s = a + b + c
+                s = a + nums[j] + nums[k]
 
                 if abs(s - target) < abs(result - target):
                     result = s
