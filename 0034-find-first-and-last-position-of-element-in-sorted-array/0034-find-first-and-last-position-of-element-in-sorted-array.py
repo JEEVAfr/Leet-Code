259156e757a00f3443ad063e5f3225e26028a727
JEEVA <146312723+JEEVAfr@users.noreply.github.com>
2024-08-31 19:16:45 +0530

    Time: 82 ms (16.08%), Space: 17.9 MB (42.95%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,14 +1,57 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
+        def findFirstPosition(nums, target):
+        # first occurence
+            l = 0
+            r = len(nums) - 1
+            first_occurrence = -1
         
-        result = [-1, -1]
+            while l <= r :
 
-        for i in range(len(nums)):
-            if nums[i] == target:
-                if result[0] == -1:  # If the first occurrence is not set, set it
-                    result[0] = i
-                result[1] = i  # Update the last occurrence to the current index
+                mid = (l + r) // 2
 
-        return result
+
+                if nums[mid] > target:
+                    r = mid - 1
+
+                elif nums[mid] < target:
+                    l = mid + 1
+
+                else: 
+                    first_occurrence = mid
+                    right = mid - 1
+
+                    r = mid - 1
+
+            return first_occurrence
+
+        def findLastPosition(nums, target):
+        # last occurence
+            l = 0
+            r = len(nums) - 1
+            last_occurence = -1
+
+            while l <= r :
+
+                mid = (l + r) // 2
+
+                if nums[mid] > target:
+                    r = mid - 1
+                elif nums[mid] < target:
+                    l = mid + 1
+                else:
+                    last_occurence = mid
+                    l = mid + 1
+        
+            return last_occurence
+    
+        first = findFirstPosition(nums, target)
+        last = findLastPosition(nums, target)
+        
+        
+        return [first, last] 
+
+        
+
 
         
