bc0d52793f0d2f52066b235e6f2f97a4a5acbdab
JEEVA <146312723+JEEVAfr@users.noreply.github.com>
2024-08-31 18:03:17 +0530

    Time: 37 ms (37.56%), Space: 16.5 MB (16.44%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.py
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -8,7 +8,7 @@ class Solution:
         
         while l <= r:
             m = (l + r) // 2
-            if isBadVersion (m) == True:
+            if isBadVersion (m):
                 r = m - 1
             else:
                 l = m + 1
