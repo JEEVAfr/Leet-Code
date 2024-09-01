b618355b5c1a6c379008b4b5e5f44b58a5301483
JEEVA <146312723+JEEVAfr@users.noreply.github.com>
2024-09-01 11:08:17 +0530

    Time: 35 ms (64.03%), Space: 16.7 MB (14.15%) - LeetHub

--- 2561-number-of-distinct-averages/2561-number-of-distinct-averages.py
+++ 2561-number-of-distinct-averages/2561-number-of-distinct-averages.py
@@ -6,19 +6,19 @@ class Solution:
         right = len(nums) - 1
 
         a = set()
-        count = 0
+        
 
         while left < right : 
             average = (nums[left] + nums[right]) / 2
 
             if average not in a:
                 a.add(average)
-                count = count + 1
+                
             
             left = left + 1
             right = right - 1
 
-        return count
+        return len(a)
 
             
 
