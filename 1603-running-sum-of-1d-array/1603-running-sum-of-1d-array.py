49daf1b477b94db40d3228ce2aaa831be25aa49f
JEEVA <146312723+JEEVAfr@users.noreply.github.com>
2024-09-02 21:45:26 +0530

    Time: 46 ms (29.14%), Space: 16.7 MB (30.76%) - LeetHub

---
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        total = 0
+        res = []
+
+        for i in nums:
+            total = total + i
+            res.append(total)
+        
+        return res
+        
