98efa010553f508967aa78beba792efeac0dbbc2
JEEVA <146312723+JEEVAfr@users.noreply.github.com>
2024-09-10 23:25:15 +0530

    Time: 74 ms (82.43%), Space: 16.6 MB (77.7%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.py
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.py
@@ -1,14 +1,10 @@
 class Solution:
     def divideArray(self, nums: List[int]) -> bool:
         nums.sort()
-        pair = []
 
         for i in range(0, len(nums), 2): # while i < len(nums) - 1
 
-            if nums[i] == nums[i + 1]:
-                pair.append((nums[i], nums[i + 1]))
-            
-            else:
+            if nums[i] != nums[i + 1]:
                 return False
               
         return True
