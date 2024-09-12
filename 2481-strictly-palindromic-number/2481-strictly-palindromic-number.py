a2187e9e692d7b0c9381d3745df8d033733350f2
JEEVA <146312723+JEEVAfr@users.noreply.github.com>
2024-09-12 23:27:09 +0530

    Time: 33 ms (66.99%), Space: 16.5 MB (17.54%) - LeetHub

---
+++ 2481-strictly-palindromic-number/2481-strictly-palindromic-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isStrictlyPalindromic(self, n: int) -> bool:
+
+        if n >= 4:
+            return False
+        elif n == 1 or n == 2:
+            return False
+        else:
+            return True
+
+        
