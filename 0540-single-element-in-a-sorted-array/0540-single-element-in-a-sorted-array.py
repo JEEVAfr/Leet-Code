7e6eb74639475027f7697a64dec89b0449e9ee01
JEEVA <146312723+JEEVAfr@users.noreply.github.com>
2024-09-12 11:25:49 +0530

    Time: 152 ms (5.18%), Space: 28.5 MB (5.9%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -10,3 +10,15 @@ class Solution:
         for key, value in count.items():
             if value == 1:
                 return key
+
+
+        """
+        unique = 0
+
+        for i in nums:
+
+            unique = unique ^ i
+        
+        return unique
+
+        """
